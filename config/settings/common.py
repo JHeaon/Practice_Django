0258f5bd7549d3640c3e8da3305e4b9436d19b04
JHeaon <j3heawon@gmail.com>
2023-12-12 23:10:15 +0900

    Feat: Add api, core app

--- config/settings/common.py
+++ config/settings/common.py
@@ -13,7 +13,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Custom apps
+    "api",
+    "core",
 ]
+
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
