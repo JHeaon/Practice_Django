b5002bd328a4e6f03f2acfcf7571dd2faf6cd1a7
JHeaon <j3heawon@gmail.com>
2023-12-13 00:09:11 +0900

    Style: black 코드 포매터 이용

--- config/settings/common.py
+++ config/settings/common.py
@@ -19,12 +19,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     # Custom apps
     "api",
     "core",
 ]
-
 
 
 MIDDLEWARE = [
